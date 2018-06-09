37275a62b81c1bb3dee28a128d58c98b72f1f4a2
someever <someever@126.com>
2018-06-09 15:46:53 +0800

    add gateway server

--- gp-component-discovery/src/main/java/com/arvin/gp/component/registry/DiscoveryApplication.java
+++ gp-component-discovery/src/main/java/com/arvin/gp/component/registry/DiscoveryApplication.java
@@ -5,15 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- *
+ * 注册中心
  * <p/>
  * @author arvin.
  * @date 2017/10/26 14:35.
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistryApplication {
+public class DiscoveryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RegistryApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 }
---
+++ gp-component-gateway/src/main/java/com/arvin/gp/component/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.arvin.gp.component.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关
+ * <p/>
+ *
+ * @author arvin.
+ * @date 2018/6/9 14:55.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
