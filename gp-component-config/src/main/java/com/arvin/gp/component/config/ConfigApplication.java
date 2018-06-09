092c5f104a9d7d2be8b207bf6176e0312f138ab5
someever <someever@126.com>
2018-06-09 13:58:04 +0800

    add config server

---
+++ gp-component-config/src/main/java/com/arvin/gp/component/config/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.arvin.gp.component.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * config server
+ * <p/>
+ *
+ * @author arvin.
+ * @date 2018/6/9 10:21.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
